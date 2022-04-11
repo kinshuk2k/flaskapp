609f536f63c3d95b5ce473f335b565e9f1598842
kinshuk2k <103109281+kinshuk2k@users.noreply.github.com>
2022-04-11 17:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
-
+import socket
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def return_hostname():
+    return "Hostname and Ip address".format(socket.gethostname(), request.remote_addr)
 
-def vmname():
-	return 'Kinshuk'
-
-if __name__ == '__main__':
-
-	app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
